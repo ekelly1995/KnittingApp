7800bcfab5d27c14897f3f4a65bc7378ca6fd739
Emily Kelly <emilykelly1995@outlook.com>
2021-09-26 21:26:10 -0600

    Add project files.

---
+++ EmilysKnittingApp/EmilysKnittingApp/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using EmilysKnittingApp.Models;
+using EmilysKnittingApp.ViewModels;
+using EmilysKnittingApp.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EmilysKnittingApp.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
